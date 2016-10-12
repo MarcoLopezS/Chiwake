7067e1ee01e5e5a34c1eecaa8e2db062783c17e0
Marco Lopez <mlopez@marostdevelopers.com>
2016-10-11 19:23:25 -0500

    texto en carta

--- resources/views/frontend/menu.blade.php
+++ resources/views/frontend/menu.blade.php
@@ -7,10 +7,15 @@
 @section('contenido_frontend')
 
 <!-- SUB BANNER -->
-<section class="sub-banner text-center section">
+<section id="carta-titulo" class="sub-banner text-center section">
     <div class="awe-parallax bg-4"></div>
     <div class="awe-title awe-title-3">
         <h3 class="lg text-uppercase">La Carta</h3>
+    </div>
+    <div id="carta-subtitulo" class="relative text-center">
+        <div class="awe-title awe-title-1">
+            <p class="lg text-uppercase">Somos especialistas en pescado y mariscos. Seleccionamos los mejores insumos para mantener la calidad de nuestros platos.</p>
+        </div>
     </div>
 </section>
 <!-- END / SUB BANNER -->
